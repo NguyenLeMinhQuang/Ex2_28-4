d68134c666cca8a1bd5c0c0c0e5d67b5c253fdda
NguyenLeMinhQuang <165896906+NguyenLeMinhQuang@users.noreply.github.com>
2024-04-28 13:32:13 +0700

    Add files via upload

---
+++ HomeScreen.js
@@ -0,0 +1,21 @@
+import { Button, Text } from 'react-native-paper';
+import {View, StyleSheet} from 'react-native'
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View style = {style.container}>
+            <Text> Home Screen </Text>
+            <Button mode ="contained" onPress={() => navigation.navigate('Details')}>
+                Go to details
+            </Button>
+        </View>
+    );
+}
+ export default HomeScreen;
+
+ const style = styleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+ });
